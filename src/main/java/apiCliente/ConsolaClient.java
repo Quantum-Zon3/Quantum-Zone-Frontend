3cc9025bddbc42f8511869710cdf9ffde94affca
Miguel Angel <lievano.miguel.3534@eam.edu.co>
2025-04-11 00:54:49 -0500

    CRUD VistaAgregarConsola terminado y posicion de la vista InicioDeSesion corregida

--- src/main/java/apiCliente/ConsolaClient.java
+++ src/main/java/apiCliente/ConsolaClient.java
@@ -3,8 +3,21 @@ package apiCliente;
 import apiService.ConsolaApiService;
 import modelo.*;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -15,11 +28,25 @@ public class ConsolaClient {
     private static ConsolaApiService consolaService;
 
     public ConsolaClient() {
-        // Configurar Retrofit
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+    	Gson gson = new GsonBuilder()
+    			.registerTypeAdapter(LocalDate.class, new JsonDeserializer<LocalDate>() {
+    				@Override
+    				public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+    					return LocalDate.parse(json.getAsString());
+    				}
+    			})
+    			.registerTypeAdapter(LocalDate.class, new JsonSerializer<LocalDate>() {
+    				@Override
+    				public JsonElement serialize(LocalDate src, Type typeOfSrc, JsonSerializationContext context) {
+    					return new JsonPrimitive(src.toString());
+    				}
+    			})
+    			.create();
+
+    		Retrofit retrofit = new Retrofit.Builder()
+    			.baseUrl(BASE_URL)
+    			.addConverterFactory(GsonConverterFactory.create(gson)) // 👈 Este Gson sí importa
+    			.build();
 
         consolaService = retrofit.create(ConsolaApiService.class);
     }
